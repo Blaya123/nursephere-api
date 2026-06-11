9271f292e0bc37974006da06cfd863bf8c244b75
nursphere-dev <nursphere@dev.com>
2026-06-11 14:23:42 +0100

    Fix Message model userId type (String instead of ObjectId) for seed users

--- src/models/Message.js
+++ src/models/Message.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
   channel: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: String, required: true },
   userName: { type: String, required: true },
   userAvatar: { type: String, default: '' },
   text: { type: String, required: true },
